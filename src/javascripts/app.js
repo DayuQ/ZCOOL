873ae33c0aff8f7a81152688db5662b14ae21015
Your Name <you@example.com>
2018-10-14 01:43:35 +0800

    basic frame && slideout

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -11,8 +11,8 @@ import home_controller from './controllers/home_controller';
 
 
 // 渲染视图
-home_controller.render(); 
-
+home_controller.render();  
+   
 // 启动路由
 const router = new Router({ initial: '#/job' });
 window.router = router;
--- src/javascripts/controllers/home_controller.js
+++ src/javascripts/controllers/home_controller.js
@@ -18,7 +18,26 @@ const render = () => {
     $('.home-title').tap(function () {
         router.switch('#/job');
     })
+
+
+    slideout();
 }
+
+const slideout = ()=>{
+    var slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70
+    });
+
+    // Toggle button
+    document.querySelector('.toggle-button').addEventListener('click', function () {
+        slideout.toggle();
+    });
+}
+
+
 
 // // 切换标签
 // const switchTab = () => {

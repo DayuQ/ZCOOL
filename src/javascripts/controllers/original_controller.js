c80483cbb7e859b3875b7bd5d99519bf19bce1ba
Your Name <you@example.com>
2018-10-15 08:48:16 +0800

    frame route change 1

--- src/javascripts/controllers/original_controller.js
+++ src/javascripts/controllers/original_controller.js
@@ -20,22 +20,24 @@ const render = () => {
     })
 
 
-    slideout();
+    // slideout();
 }
 
-const slideout = ()=>{
-    var slideout = new Slideout({
-        'panel': document.getElementById('panel'),
-        'menu': document.getElementById('menu'),
-        'padding': 256,
-        'tolerance': 70
-    });
+
+
+// const slideout = ()=>{
+//     var slideout = new Slideout({
+//         'panel': document.getElementById('panel'),
+//         'menu': document.getElementById('menu'),
+//         'padding': 256,
+//         'tolerance': 70 
+//     });
 
     // Toggle button
-    document.querySelector('.toggle-button').addEventListener('click', function () {
-        slideout.toggle();
-    });
-}
+    // document.querySelector('.toggle-button').addEventListener('click', function () {
+    //     slideout.toggle();
+    // });
+// }
 
 
 

bc1d9c866979f8755345d6fb9bd8fc4a82a05776
miaoketang <2542998441@qq.com>
2018-10-22 21:36:24 +0800

    add RegExp of register&login

--- src/javascripts/controllers/login_controller.js
+++ src/javascripts/controllers/login_controller.js
@@ -12,15 +12,67 @@ const render =   () => {
 
 const changeLoginType = ()=>{
 
+    
+    //密码登录,验证了用户名，密码非空即可
+    $('.login-password').on('input change',function(){
+         let myUsername=localStorage.getItem("username")
+         console.log(myUsername);
+         
+         //非空按钮变色
+         if($('.login-password p.username input').val()&&$('.login-password p.password input').val()){ 
+             document.querySelector('.login-password .login-btn').style.background="#ffd100"
+             //验证登录
+             if($('.login-password p.username input').val()==myUsername&&$('.login-password p.password input').val()){ 
+                document.querySelector('.login-password .login-btn').style.background="#ffd100"
+                $('.login-btn').on('tap',function(){
+                   $("#tel-loginForm").submit();
+                })
+            }
+             
+         }else{
+            document.querySelector('.login-password .login-btn').style.background=""
+         }
+          
+     })
+ 
+     //电话号登录，顺带注册，做了电话号码的正则验证，验证码非空即可
+     $('.login-phone').on('input change',function(){
+
+        let sMobile=$('.login-phone p.phone input').val()
+        // console.log(sMobile);
+        // console.log((/^1(?:3\d|4[4-9]|5[0-35-9]|6[67]|7[013-8]|8\d|9\d)\d{8}$/.test(sMobile)));
+
+         if(/^1(?:3\d|4[4-9]|5[0-35-9]|6[67]|7[013-8]|8\d|9\d)\d{8}$/.test(sMobile)){
+             document.querySelector('.login-phone .login-btn').style.background="#ffd100"
+             
+             if($('.login-phone p.verify input').val()){
+                $('.login-btn').on('tap',function(){
+                    localStorage.setItem("username",sMobile);
+    
+                    console.log(localStorage.getItem("username"));
+                    
+                    $("#tel-loginForm").submit();
+                 })
+             }
+         }else{
+            document.querySelector('.login-phone .login-btn').style.background=""
+         }
+          
+     })
+
     $('.login-type-phone').on('tap',function(){
         document.querySelector('.login-phone').style.display = "none";
         document.querySelector('.login-password').style.display = "block"
     })
 
     $('.login-type-password').on('tap',function(){
+        
         document.querySelector('.login-phone').style.display = "block";
         document.querySelector('.login-password').style.display = "none"
+        
     })
+   
+    
 }
 
 
